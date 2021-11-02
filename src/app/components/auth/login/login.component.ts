8484c514b9fd802f5c9e73f8145a02212cc65128
Bartlomiej Sikora <bartlsikora@gmail.com>
2021-11-02 19:23:27 +0100

    Added login component

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  formSubmitted = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      login: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required]
+    });
+  }
+
+  submit() {
+    console.log('Form submitted!');
+    console.log(this.loginForm.value);
+    this.formSubmitted = true;
+  }
+}
